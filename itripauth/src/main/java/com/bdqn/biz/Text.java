aaa97f218a41604e76d2d1ddc483529bc88ebf88
lixinhong01 <356489791@qq.com>
2019-08-17 15:19:18 +0800

    爱旅行项目

--- itripauth/src/main/java/com/bdqn/biz/Text.java
+++ itripauth/src/main/java/com/bdqn/biz/Text.java
@@ -7,6 +7,6 @@ public class Text {
         //生成四位随机数
         Random ne=new Random();//实例化一个random的对象ne
         int x=ne.nextInt(9999-1000+1)+1000;//为变量赋随机值1000-9999
-        System.out.println("产生的随机数是:"+x);//输出
+        System.out.println("产生的随机数是:"+x);
     }
 }
