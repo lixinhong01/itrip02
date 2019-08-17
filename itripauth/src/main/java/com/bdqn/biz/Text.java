c2fdd8e1d755f6c841afcc247e9afc38ac349478
lixinhong01 <356489791@qq.com>
2019-08-17 14:08:39 +0800

    爱旅行项目

---
+++ itripauth/src/main/java/com/bdqn/biz/Text.java
@@ -0,0 +1,11 @@
+package com.bdqn.biz;
+
+import java.util.Random;
+
+public class Text {
+    public static void main(String[] args) {
+        Random ne=new Random();//实例化一个random的对象ne
+        int x=ne.nextInt(9999-1000+1)+1000;//为变量赋随机值1000-9999
+        System.out.println("产生的随机数是:"+x);//输出
+    }
+}
